ea5234a6cdab24e48b36bfdcb552e8bd0664df54
eye <eye@eye-desktop.(none)>
2010-05-05 13:02:35 -0400

    Misc formatting and removed debug print statements.

--- drivers/video/omap2/displays/panel-cmel-oled43.c
+++ drivers/video/omap2/displays/panel-cmel-oled43.c
@@ -22,11 +22,11 @@
 
 #include <plat/display.h>
 
-#define CS_PIN			139
-#define MOSI_PIN		144
-#define CLK_PIN			138
-#define RESET_PIN		137
-#define PANEL_PWR_PIN	143
+#define CS_PIN          139
+#define MOSI_PIN        144
+#define CLK_PIN         138
+#define RESET_PIN       137
+#define PANEL_PWR_PIN   143
 
 #define digitalWrite(PIN,VALUE) (gpio_direction_output(PIN,VALUE))
 
@@ -41,6 +41,15 @@
 #define PANEL_PWR_LOW digitalWrite(PANEL_PWR_PIN, 0)
 #define PANEL_PWR_HIGH digitalWrite(PANEL_PWR_PIN, 1)
 
+/*
+ *   Video Timings
+ *       
+ *   Frame Rate = 1/((vbp + vfp + 272)*(hbp + hfp + 480)/pixel_clock)
+ *   Where hfp must be > 30,
+ *         hbp must be = 102,
+ *         vfp must be > 10,
+ *         vbp must be = 20,
+ */
 static struct omap_video_timings oled43_timings = {
 	.x_res = 480,
 	.y_res = 272,
@@ -54,11 +63,6 @@ static struct omap_video_timings oled43_timings = {
 	.vsw		= 3,
 	.vfp		= 11,
 	.vbp		= 20,
-// frame_rate = 1/((vbp + vfp + 272)*(hbp + hfp + 480)/pixel_clock)
-// where hfp must be > 30
-//       hbp must be = 102
-//       vfp must be > 10
-//       vbp must be = 20
 };
 
 ////////////////////////////////////////////////////////
@@ -92,8 +96,7 @@ uint8_t bit;
 // Write a value to the OLED panel
 static void oled43_writeReg(uint8_t index, uint8_t val) {
 
-	pr_info("cmel_oled43_panel: Sending data %x,%x \n", index, val);
-//    fprintf(stdout, "Sending data %x,%x \n", index, val);
+	//pr_info("cmel_oled43_panel: Sending data %x,%x \n", index, val);
 
     CS_LOW;
     oled43_spi_write8((index << 1));
@@ -103,7 +106,7 @@ static void oled43_writeReg(uint8_t index, uint8_t val) {
 
 static int oled43_hardware_init(void) {
 
-	/* Panel init sequence from the cmel panel datasheet */
+	/* Panel init sequence from the panel datasheet */
 	PANEL_PWR_LOW;						// just to be sure, hold the oled power supply off
 	RESET_LOW;							// panel in reset
 	oled43_spi_init();					// init spi interface
@@ -187,7 +190,7 @@ err0:
 
 static void oled43_panel_power_off(struct omap_dss_device *dssdev)
 {
-	PANEL_PWR_LOW; //simply turn off the power supply
+	PANEL_PWR_LOW; //turn off the power supply
 
 	if (dssdev->platform_disable)
 		dssdev->platform_disable(dssdev);
@@ -197,7 +200,7 @@ static void oled43_panel_power_off(struct omap_dss_device *dssdev)
 
 static int oled43_panel_probe(struct omap_dss_device *dssdev)
 {
-	dssdev->panel.config = OMAP_DSS_LCD_TFT; // | OMAP_DSS_LCD_IVS | OMAP_DSS_LCD_IHS;
+	dssdev->panel.config = OMAP_DSS_LCD_TFT;
 	dssdev->panel.timings = oled43_timings;
 
 	return 0;
@@ -291,14 +294,6 @@ static int __init oled43_panel_drv_init(void)
 	int ret = 0;
 
 	printk(KERN_INFO "cmel_oled43_panel: init panel\n");
-
-    //configure the gpio
-//	omap_cfg_reg(AH3_34XX_GPIO137_OUT);     //NRESET
-//	omap_cfg_reg(AF3_34XX_GPIO138_OUT);		//soft CLK
-//	omap_cfg_reg(AE3_34XX_GPIO139_OUT);		//soft CS
-//	omap_cfg_reg(AE5_34XX_GPIO143_OUT);		//Panel_Pwr
-//	omap_cfg_reg(AB26_34XX_GPIO144_OUT);	//soft MOSI
-//	omap_cfg_reg(AA25_34XX_GPIO146_UP);     //soft MISO
 
 	//Get the GPIO pins used for the panel
 	gpio_request(CS_PIN, "OLED43_CS_PIN");
