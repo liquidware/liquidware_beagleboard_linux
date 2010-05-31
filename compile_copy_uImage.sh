3193cc56b0fb5098a4680986d3a1e4d765094612
cladden <avrman@gmail.com>
2010-05-31 16:16:13 -0400

    misc script cleanups

--- compile_copy_uImage.sh
+++ compile_copy_uImage.sh
@@ -1,45 +1,67 @@
 #!/bin/sh
 
-MOD_PATH=/tmp/bbext
-MOD_DEV=/dev/sdb2
+#config parameters
+FAT_DEVICE=/dev/sdb1
+ROOTFS_DEVICE=/dev/sdb2
 
-FAT_PATH=/tmp/bbfat
-FAT_DEV=/dev/sdb1
+ROOTFS_MOUNT=/tmp/bbext
+FAT_MOUNT=/tmp/bbfat
 
-compileImage="sudo make -j8 INSTALL_MOD_PATH=${MOD_PATH} ARCH=arm uImage CROSS_COMPILE=/home/eye/CodeSourcery/Sourcery_G++_Lite/bin/arm-none-linux-gnueabi-"
+COMPILER_INSTALL=/home/eye/CodeSourcery/Sourcery_G++_Lite/bin/arm-none-linux-gnueabi-
 
-compileModules="sudo make -j8 INSTALL_MOD_PATH=${MOD_PATH} ARCH=arm modules CROSS_COMPILE=/home/eye/CodeSourcery/Sourcery_G++_Lite/bin/arm-none-linux-gnueabi-"
+#helper commands
+menuconfig="sudo make -j8 INSTALL_ROOTFS_MOUNT=${ROOTFS_MOUNT} ARCH=arm menuconfig CROSS_COMPILE=${COMPILER_INSTALL}"
 
-installModules="sudo make -j8 INSTALL_MOD_PATH=${MOD_PATH} ARCH=arm KERNEL_CONFIGS=DEBUG uImage modules modules_install CROSS_COMPILE=/home/eye/CodeSourcery/Sourcery_G++_Lite/bin/arm-none-linux-gnueabi-"
+compileImage="sudo make -j8 INSTALL_ROOTFS_MOUNT=${ROOTFS_MOUNT} ARCH=arm uImage CROSS_COMPILE=${COMPILER_INSTALL}"
 
-#echo "unmounting"
-#sudo umount $MOD_DEV
-#sudo umount $FAT_DEV
-sleep 1
+compileModules="sudo make -j8 INSTALL_ROOTFS_MOUNT=${ROOTFS_MOUNT} ARCH=arm modules CROSS_COMPILE=${COMPILER_INSTALL}"
+
+installModules="sudo make -j8 INSTALL_ROOTFS_MOUNT=${ROOTFS_MOUNT} ARCH=arm KERNEL_CONFIGS=DEBUG uImage modules modules_install CROSS_COMPILE=${COMPILER_INSTALL}"
+
+if [ "$1" == "menuconfig" ]; then
+theCommand=$menuconfig
+ elif [ "$1" == "compileImage" ]; then
+    theCommand=$compileImage
+ elif [ "$1" == "compileModules" ]; then
+    theCommand=$compileModules
+ elif [ "$1" == "installModules" ]; then
+    theCommand=$installModules
+ else
+    echo "Unknown Command, exiting."
+    exit
+fi
+
+echo $theCommand
+
+echo "unmounting"
+sudo umount $ROOTFS_DEVICE
+sudo umount $FAT_DEVICE
+
 echo "mounting"
-#sudo mount $FAT_DEV $FAT_PATH
-#sudo mount $MOD_DEV $MOD_PATH
+sudo mount $FAT_DEVICE $FAT_MOUNT
+sudo mount $ROOTFS_DEVICE $ROOTFS_MOUNT
 
 echo "Compiling Image"
-if  $installModules; then
-        echo "Copying uImage to ${FAT_PATH}"    
-        sudo cp ./arch/arm/boot/uImage $FAT_PATH/uImage
+if $theCommand 
+then
+        echo "Copying uImage to ${FAT_MOUNT}"    
+        sudo cp ./arch/arm/boot/uImage $FAT_MOUNT/uImage
 
-        cd $FAT_PATH
+        cd $FAT_MOUNT
         echo "Syncing data"
         sync
         cd ..
-        sleep 2       
+        sleep 1       
         
 
-        cd $MOD_PATH
+        cd $ROOTFS_MOUNT
         echo "Syncing data"
         sync
         cd ..
-        sleep 2
+        sleep 1
 
-        #sudo umount $FAT_DEV
-        #sudo umount $MOD_DEV        
+        sudo umount $FAT_DEVICE
+        sudo umount $ROOTFS_DEVICE        
         
         echo 'Done.'
         exit
